f99eb4abfc84448367d569ae4b361066850a2cdb
Mahdi <m.karim7804@gmail.com>
2024-09-11 18:47:51 +0330

    Add IUnitOfWork interface to the domain layer

---
+++ TestTask.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask.Domain.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IProductRepository ProductRepository { get; }
+        IRefreshTokenRepository RefreshTokenRepository { get; }
+
+        //IManufacturerRepository ManufacturerRepository { get; }       //Manufacturer inherit from IdentityUser and it by default use UnitOfWork
+
+        Task<int> SaveChangesAsync();
+    }
+}
